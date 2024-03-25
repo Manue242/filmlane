d35b8326a9e203f78a3583ded918f9b3d01bff22
emmanuelkwedi <161734689+emmanuelkwedi@users.noreply.github.com>
2024-03-25 21:53:03 +0100

    Update Trigger.sql

--- filmlane-master/sql/Trigger.sql
+++ filmlane-master/sql/Trigger.sql
@@ -1,75 +1,87 @@
 DELIMITER //
 
-create or replace trigger vérifie_date_naissanceacteur
-before insert or update on Acteur
-for each row
-begin
-    if new.date_naiss_acteur > CURDATE() then
-        SIGNAL SQLSTATE '45000'
-        set MESSAGE_TEXT = 'La date de naissance ne peut pas être supérieure à la date actuelle.';
-    end if;
-end;
-
+CREATE TRIGGER before_insert_update_acteur
+BEFORE INSERT ON Acteur
+FOR EACH ROW
+BEGIN
+    CALL verifie_date_naissance_acteur(NEW.date_naiss_acteur);
+END;
 //
 
 DELIMITER ;
+
 
 DELIMITER //
 
-create or replace trigger vérifie_date_naissancerealisateur
-before insert or update on Réalisateur
-for each row
-begin
-    if new.date_naiss_rea > CURDATE() then
-        signal sqlstate '45000'
-        set MESSAGE_TEXT = 'La date de naissance ne peut pas être supérieure à la date actuelle.';
-    end if;
-end;
+CREATE TRIGGER before_update_acteur
+BEFORE UPDATE ON Acteur
+FOR EACH ROW
+BEGIN
+    CALL verifie_date_naissance_acteur(NEW.date_naiss_acteur);
+END;
+//
 
+
+
+DELIMITER //
+
+
+DELIMITER //
+
+CREATE TRIGGER before_insert_update_realisateur
+BEFORE INSERT ON Realisateur
+FOR EACH ROW
+BEGIN
+    CALL verifie_date_naissance_acteur(NEW.date_naiss_rea);
+END;
 //
 
 DELIMITER ;
 
 
-CREATE OR REPLACE PROCEDURE InsererUtilisateur(
-    p_Id_user INT,
-    p_nom_user VARCHAR2,
-    p_mot_de_passe VARCHAR2,
-    p_email VARCHAR2
+DELIMITER //
+
+CREATE PROCEDURE InsererUtilisateur(
+    IN p_Id_user INT,
+    IN p_nom_user VARCHAR(255),
+    IN p_mot_de_passe VARCHAR(255),
+    IN p_email VARCHAR(255)
 )
-AS
 BEGIN
     INSERT INTO Utilisateur (Id_user, nom_user, mot_de_passe, email)
     VALUES (p_Id_user, p_nom_user, p_mot_de_passe, p_email);
-    
-    COMMIT; -- Confirmer la transaction
-END InsererUtilisateur;
-/
+END;
+//
 
+DELIMITER ;
 
-CREATE OR REPLACE PROCEDURE InsererParticipationDebat(
-    p_Id_user INT,
-    p_Id_debat INT
+CALL InsererUtilisateur(1, 'John', 'motdepasse', 'john@example.com');
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS InsererParticipationDebat;
+CREATE PROCEDURE InsererParticipationDebat(
+    IN p_Id_user INT,
+    IN p_Id_debat INT
 )
-AS
 BEGIN
     INSERT INTO ParticipationDebat (Id_user, Id_debat)
     VALUES (p_Id_user, p_Id_debat);
-    
-    COMMIT; -- Confirmer la transaction
-END InsererParticipationDebat;
-/
+END;
+//
+
+DELIMITER ;
 
 
-CREATE OR REPLACE PROCEDURE InsererParticipationEven(
-    p_Id_even INT,
-    p_Id_user INT
+DELIMITER //
+
+CREATE PROCEDURE InsererParticipationDebat(
+    p_Id_user INT,
+    p_Id_debat INT
 )
-AS
 BEGIN
-    INSERT INTO ParticipationEven (Id_even, Id_user)
-    VALUES (p_Id_even, p_Id_user);
-    
-    COMMIT; -- Confirmer la transaction
-END InsererParticipationEven;
-/
+    INSERT INTO ParticipationDebat (Id_user, Id_debat)
+    VALUES (p_Id_user, p_Id_debat);
+END //
+
+DELIMITER ;
